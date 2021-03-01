bfa2df663c12fe5c8da9cd00380a0f277dfbea91
contact@kenada.fr <contact@kenada.fr>
2021-03-02 00:51:28 +0100

    Intégration d'un petit framework perso

---
+++ framework-perso.js
@@ -0,0 +1,23 @@
+
+/* Framework Perso */
+
+// Quick call of querySelector
+
+const $ = document.querySelector;
+
+// Cross Browser implementation of Event Listener
+// obj = HTMLElement
+// event = string (exemple : click, change, ...)
+// func = function(event) { ... }
+
+const _addEventListener = function(obj, event, func) {
+	// W3C
+    if(obj.addEventListener) {
+		obj.addEventListener(event, func, false);
+		return true;
+	}
+	// Microsoft model
+	else if(obj.attachEvent) {
+		return obj.attachEvent("on"+ event, func);
+	}
+};
